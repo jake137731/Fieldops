f8af66be7ce1f795665a8211338bab0b2572f7a3
jake137731 <moniker13@protonmail.com>
2025-06-14 11:32:11 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,23 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Backend running on ${PORT}`));
+const express = require('express');
+const Procore = require('@procore/js-sdk');
+
+const app = express();
+const PORT = 8000;
+
+app.get('/auth', (req, res) => {
+  const client = new Procore.Client({
+    clientID: tvTY_UFuDzFNhucSI
+    clientSecret: QwOiCgzPYYQxqvB
+    redirectUri: https://fieldops360.com/oauth/callback
+  });
+
+  const authUrl = client.getAuthorizationUrl(['openid']);
+  res.redirect(authUrl);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
